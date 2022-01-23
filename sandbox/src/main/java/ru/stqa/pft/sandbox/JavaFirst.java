03de1c5ea2bdb0a9bb1437c05495fb25dc5c87fc
George <nolife320@gmail.com>
2022-01-23 17:47:10 +0300

    Конец 1 занятия

--- sandbox/src/main/java/ru/stqa/pft/sandbox/JavaFirst.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/JavaFirst.java
@@ -4,9 +4,15 @@ public class JavaFirst {
 
 	public static void main(String[] args){
 		hello("GF");
-		System.out.println(area(5));
 
-		System.out.println(area2(2,4));
+		Square s = new Square(5);
+		System.out.println(s.area());
+
+		Rectangle r = new Rectangle(4,6);
+		System.out.println(r.area());
+
+
+
 
 
 
@@ -17,11 +23,7 @@ public class JavaFirst {
 		System.out.println("Hello " + somebody);
 	}
 
-	public static double area (double l){
-		return  l * l;
-	}
 
-	public static double area2 (double a, double b){
-		return  a*b;
-	}
+
+
 }
