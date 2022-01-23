4a7c1e2745676a60257dbc950e359a779bfec851
George <nolife320@gmail.com>
2022-01-23 17:25:57 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/ru/stqa/pft/sandbox/JavaFirst.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/JavaFirst.java
@@ -3,8 +3,25 @@ package ru.stqa.pft.sandbox;
 public class JavaFirst {
 
 	public static void main(String[] args){
+		hello("GF");
+		System.out.println(area(5));
 
-		System.out.println("hello");
+		System.out.println(area2(2,4));
 
+
+
+
+	}
+
+	public static void hello (String somebody){
+		System.out.println("Hello " + somebody);
+	}
+
+	public static double area (double l){
+		return  l * l;
+	}
+
+	public static double area2 (double a, double b){
+		return  a*b;
 	}
 }
