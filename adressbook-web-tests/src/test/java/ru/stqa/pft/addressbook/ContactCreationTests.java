ef03d31d63b6efa3698cd10f8a5ed917978e93c3
George <nolife320@gmail.com>
2022-05-29 14:03:45 +0300

    pull up gropDel and ContactCreation

---
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
@@ -0,0 +1,37 @@
+package ru.stqa.pft.addressbook;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.testng.annotations.Test;
+
+public class ContactCreationTests extends TestBase{
+
+
+  @Test
+  public void testContactCreation() throws Exception {
+    newContact();
+    fillContactForm(new ContactData("namef", "nameM", "nameL", "nick", "title", "company", "address", "tel home", "mobile", "work", "fax", "email", "email2", "email3", "homepage"));
+    submitNewContact();
+    goToHomePage();
+    logout();
+  }
+
+//  private boolean isElementPresent(By by) {
+//    try {
+//      wd.findElement(by);
+//      return true;
+//    } catch (NoSuchElementException e) {
+//      return false;
+//    }
+//  }
+//
+//  private boolean isAlertPresent() {
+//    try {
+//      wd.switchTo().alert();
+//      return true;
+//    } catch (NoAlertPresentException e) {
+//      return false;
+//    }
+//  }
+}
--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/TestBase.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/TestBase.java
@@ -11,7 +11,8 @@ import java.util.concurrent.TimeUnit;
 public class TestBase {
 
 
-  public WebDriver wd;
+
+  protected WebDriver wd;
 
   @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
@@ -21,10 +22,44 @@ public class TestBase {
     login("admin", "secret");
   }
 
+
+
+
+
   private void login(String username, String password) {
     wd.findElement(By.name("user")).sendKeys(username);
     wd.findElement(By.name("pass")).sendKeys(password);
     wd.findElement(By.xpath("//input[@value='Login']")).click();
+  }
+
+  protected void newContact() {
+    wd.findElement(By.linkText("add new")).click();
+  }
+
+  protected void fillContactForm(ContactData contactData) {
+    wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
+    wd.findElement(By.name("middlename")).sendKeys(contactData.getMiddlename());
+    wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
+    wd.findElement(By.name("nickname")).sendKeys(contactData.getNickname());
+    wd.findElement(By.name("title")).sendKeys(contactData.getTitle());
+    wd.findElement(By.name("company")).sendKeys(contactData.getCompany());
+    wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
+    wd.findElement(By.name("home")).sendKeys(contactData.getTel_home());
+    wd.findElement(By.name("mobile")).sendKeys(contactData.getTel_mobile());
+    wd.findElement(By.name("work")).sendKeys(contactData.getTel_work());
+    wd.findElement(By.name("fax")).sendKeys(contactData.getFax());
+    wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
+    wd.findElement(By.name("email2")).sendKeys(contactData.getEmail2());
+    wd.findElement(By.name("email3")).sendKeys(contactData.getEmail3());
+    wd.findElement(By.name("homepage")).sendKeys(contactData.getHomepage());
+  }
+
+  protected void submitNewContact() {
+    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+  }
+
+  protected void goToHomePage() {
+    wd.findElement(By.linkText("home page")).click();
   }
 
   protected void logout() {
@@ -53,6 +88,7 @@ public class TestBase {
     wd.findElement(By.xpath("//form[@action='/addressbook/group.php']")).click();
   }
 
+
   @AfterMethod(alwaysRun = true)
   public void tearDown() throws Exception {
     wd.quit();
@@ -65,4 +101,6 @@ public class TestBase {
   protected void selectGroup() {
     wd.findElement(By.name("selected[]")).click();
   }
+
+
 }
